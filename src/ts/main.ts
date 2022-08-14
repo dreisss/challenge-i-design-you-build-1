49768ed313accdd15c73f4244ea2d0b0c118b198
Davi Reis <reis.davi11010@gmail.com>
2022-08-14 00:08:58 -0300

    feat: styled basic mobile page

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,3 @@
 import "https://unpkg.com/phosphor-icons@1.3.2";
+import "./fonts.ts";
 import "../styles/app.css";
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "background-from": "#2C4550",
+        "background-to": "#0C0C0C",
+      },
+
+      fontFamily: {
+        poppins: ["'Poppins'", "sans-serif"],
+      },
+
+      backgroundImage: {
+        background: "url('/assets/background.png')",
+      },
+
+      backgroundSize: {
+        size: "auto 50%",
+      },
+
+      backgroundPosition: {
+        position: "right -60vw top 9rem",
+      },
+    },
   },
   plugins: [],
 };
